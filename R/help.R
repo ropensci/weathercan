0f6372d0e7bd173a9562923ad30cf69711eb3429
steffilazerte <steffi@steffi.ca>
2016-06-23 12:39:39 -0700

    Add package scale documentation placeholder

---
+++ R/help.R
@@ -0,0 +1,9 @@
+#' Easy downloading of weather data from Environment Canada
+#'
+#' Extended documentation coming soon... see index for details.
+#'
+#' @references Environment Canada \url{}.
+#' @docType package
+#' @name envirocan-package
+#' @aliases envirocan envirocan-package
+NULL
