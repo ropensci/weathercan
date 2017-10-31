77259a8660d5c1a25f5cdb0c8e8968e1c86727d3
Stefanie LaZerte <steffi@steffi.ca>
2017-10-31 10:00:52 -0500

    Add details linking to vignettes

--- R/weather.R
+++ R/weather.R
@@ -1,7 +1,8 @@
 #' Download weather data from Environment Canada
 #'
 #' Downloads data from Environment Canada for one or more stations. For details
-#' and units, see the glossary online
+#' and units, see the glossary vignette (\code{vignette("glossary", package =
+#' "weathercan")}) or the glossary online
 #' \url{http://climate.weather.gc.ca/glossary_e.html}.
 #'
 #' @details Data can be returned 'raw' (format = FALSE) or can be formatted.
