ffeb8d86a58910edca4bfb1efa3c1a1fad40453d
Stefanie LaZerte <steffi@steffi.ca>
2018-06-27 14:59:32 -0500

    Prepare for CRAN submission

--- cran-comments.md
+++ cran-comments.md
@@ -1,21 +1,19 @@
 ## Test environments
-As of June 09, 2017
+As of June 27, 2018
 
-Local
-* Windows 10 (R 3.3.3)
-* ubuntu 16.04 (R 3.4.0)
-
-Travis CI
-* ubuntu 12.04, 14.04 (R 3.3.0 and devel)
-* OSX 10.11.6, 10.12 (R 3.3.0 and devel)
-
-Appveyor
-* Windows (R 3.3.0 and devel, i386 and x64)
-
+* Local - ubuntu 16.04 (R 3.4.4), Windows 10 (R 3.5.0)
+* Travis CI - ubuntu 14.04 (R 3.4.4, 3.5.0, and devel)
+* Travis CI - OSX 10.12.6 (R 3.5.0)
+* Appveyor - Windows Server 2012 (R 3.4.4, 3.5.0 (release), 3.5.0 (patched), all on both i386 and x64)
+* win-builder (devel and release)
 
 ## R CMD check results
 
-There were no ERRORs, no WARNINGs and no NOTES.
+There were no ERRORs, no WARNINGs
+
+1 NOTE: 
+Maintainer: ‘Steffi LaZerte <steffi@steffi.ca>’
+New submission
 
 ## Downstream dependencies
 
---
+++ release_script.R
@@ -0,0 +1,40 @@
+# Steps/Commands to run before a CRAN release -----------------------------
+
+## Check if version is appropriate
+# http://shiny.andyteucher.ca/shinyapps/rver-deps/
+
+## Internal data files
+source("data-raw/data-raw.R")
+source("data-raw/data-index.R")
+
+## Checks
+devtools::check()     # Local
+devtools::build_win() # Win builder
+
+library(rhub)
+check_on_debian()
+check_on_windows()
+check_on_ubuntu()
+check_on_macos()
+
+## Run in console
+system("cd ..; R CMD build weathercan")
+system("cd ..; R CMD check weathercan_0.2.7.tar.gz --as-cran")
+
+## Update codemeta
+codemetar::write_codemeta()
+
+## Documentation
+# Update NEWS
+# Update cran-comments
+
+## Update website
+## Push to github
+## Check travis / appveyor
+
+## Actually release it (SEND TO CRAN!)
+devtools::release()
+
+## Once it is released (Accepted by CRAN) create signed release on github
+system("git tag -s v0.2.7 -m 'v0.2.7'")
+system("git push --tags")
