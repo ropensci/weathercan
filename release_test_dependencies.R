bc6e112523b687999d1098a7fc06ea19671e1137
Steffi LaZerte <steffi@steffi.ca>
2019-09-23 20:43:42 -0500

    Add rlang to test

--- release_test_dependencies.R
+++ release_test_dependencies.R
@@ -2,7 +2,9 @@
 
 # Test min version of tidyr
 remotes::install_version("tidyr", "0.8.0")
+remotes::install_version("rlang", "0.3.2")
 packageVersion("tidyr")
+packageVersion("rlang")
 devtools::test()
 install.packages("tidyr")
 
