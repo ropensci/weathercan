1903330abea86dfdb2c714937cf74e95b9289b5f
Steffi LaZerte <steffi@steffi.ca>
2020-01-06 16:41:19 -0600

    Clarify messages

--- tests/testthat/test_08_normals.R
+++ tests/testthat/test_08_normals.R
@@ -10,9 +10,16 @@ test_that("normals_url() correctly creates urls", {
   expect_error(normals_url(prov = "MB", climate_id = c("5010480", "5010480"),
                            normals_years = "1971-2000"), "Climate normals are not")
 
-  expect_silent(h <- httr::GET(normals_url(prov = "MB", climate_id = "5010480",
-                                           normals_years = "1981-2010")))
-    expect_equal(httr::status_code(h), 200)
+  expect_silent(normals_url(prov = "MB", climate_id = "5010480",
+                            normals_years = "1981-2010"))
+
+  bkup <- getOption("weathercan.urls.normals")
+  options(weathercan.urls.normals = "https://httpstat.us/404")
+  expect_error(normals_url(prov = "MB", climate_id = "5010480",
+                           normals_years = "1981-2010"),
+               "Failed to access",
+               class = "http_error")
+  options(weathercan.urls.normals = bkup)
 })
 
 test_that("normals_raw() download normals as character", {
@@ -20,6 +27,10 @@ test_that("normals_raw() download normals as character", {
                 "1981-2010/MB/climate_normals_MB_5010480_1981-2010.csv")
   expect_silent(normals_raw(url)) %>%
     expect_is("character")
+
+  expect_error(normals_raw("https://httpstat.us/404"),
+               "Failed to access climate normals",
+               class = "http_error")
 })
 
 test_that("normals_extract() cleans up raw data", {
