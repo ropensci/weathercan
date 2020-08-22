c9e00cb8d8a4fe6afa0ad16a817ac83d78e4066f
Steffi LaZerte <steffi@steffi.ca>
2020-08-22 13:42:07 -0500

    Don't test for name changes on CRAN

--- tests/testthat/test_05_weather_names.R
+++ tests/testthat/test_05_weather_names.R
@@ -4,12 +4,11 @@ context("Data column names")
 # Test and alert only if ECCC changes any of the column names
 test_that("weather_raw() hour column names as expected", {
 
-  vcr::use_cassette("weather_names1", {
-    wd <- weather_html(station_id = 51423,
-                       date = as.Date("2014-01-01"),
-                       interval = "hour") %>%
-      weather_raw()
-  })
+  skip_on_cran()
+  wd <- weather_html(station_id = 51423,
+                     date = as.Date("2014-01-01"),
+                     interval = "hour") %>%
+    weather_raw()
 
   expect_true(all(names(wd) %in% c(w_names$hour,
                                    "Latitude (y)", "Longitude (x)",
@@ -21,13 +20,11 @@ test_that("weather_raw() hour column names as expected", {
 })
 
 test_that("weather_raw() day column names as expected", {
-
-  vcr::use_cassette("weather_names2", {
-    wd <- weather_html(station_id = 51423,
-                       date = as.Date("2014-01-01"),
-                       interval = "day") %>%
-      weather_raw()
-  })
+  skip_on_cran()
+  wd <- weather_html(station_id = 51423,
+                     date = as.Date("2014-01-01"),
+                     interval = "day") %>%
+    weather_raw()
 
   expect_true(all(names(wd) %in% c(w_names$day,
                                    "Latitude (y)", "Longitude (x)",
@@ -36,13 +33,11 @@ test_that("weather_raw() day column names as expected", {
 })
 
 test_that("weather_raw() month column names as expected", {
-
-  vcr::use_cassette("weather_names3", {
-    wd <- weather_html(station_id = 43823,
-                       date = as.Date("2005-01-01"),
-                       interval = "month") %>%
-      weather_raw()
-  })
+  skip_on_cran()
+  wd <- weather_html(station_id = 43823,
+                     date = as.Date("2005-01-01"),
+                     interval = "month") %>%
+    weather_raw()
 
   expect_true(all(names(wd) %in% c(w_names$month,
                                    "Latitude (y)", "Longitude (x)",
