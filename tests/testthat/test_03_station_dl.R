2bcad0938508d2760a96f354dd4066bb8c429b65
steffilazerte <steffi@steffi.ca>
2021-04-16 18:28:57 -0500

    For tests use ext

--- tests/testthat/test_03_station_dl.R
+++ tests/testthat/test_03_station_dl.R
@@ -45,11 +45,15 @@ test_that("stations_dl() runs and updates data", {
   #vcr::use_cassette("stations_dl_good", {  # Don't use vcr until deal with url redirects
   #})
   stub(stations_dl_internal, "Sys.Date", as.Date("2000-01-01"))
-  stations_dl_internal()
+  expect_message(
+    stations_dl_internal(loc = system.file("extdata", package = "weathercan")),
+    "Stations data saved")
   expect_equal(stations_meta()$weathercan_modified, as.Date("2000-01-01"))
 
   stub(stations_dl_internal, "Sys.Date", Sys.Date())
-  expect_message(stations_dl_internal(), "Stations data saved")
+  expect_message(
+    stations_dl_internal(loc = system.file("extdata", package = "weathercan")),
+    "Stations data saved")
   expect_equal(stations_meta()$weathercan_modified, Sys.Date())
 })
 
