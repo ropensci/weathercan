93cf21823d1e9e1e694ecd892aad9a475d54b709
steffilazerte <steffi@steffi.ca>
2021-08-23 13:36:11 -0500

    typo

--- R/weathercan-pkg.R
+++ R/weathercan-pkg.R
@@ -1,7 +1,7 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
     "weathercan v", utils::packageVersion("weathercan"), "\n",
-    "The internal `stations`data has been ",
+    "The internal `stations` data has been ",
     "deprecated in favour of the function `stations()`.\n",
     "See ?stations for more details.")
 }
